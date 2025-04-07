ef602919ccde550b640a665e6016cf576e43ba91
scossar <scossar3@gmail.com>
2025-04-06 19:21:51 -0700

    add cubic interpolation to static delay

--- src/delay1_cubic~.c
+++ src/delay1_cubic~.c
@@ -1,38 +1,28 @@
 #include "simple_del_shared.h"
 #include <m_pd.h>
 
-/* The relevant part of simple_del_shared:
-* #ifndef SIMPLE_DEL_SHARED_H
-* #define SIMPLE_DEL_SHARED_H
-*
-* #include "m_pd.h"
-*
-* #define XTRASAMPS 4
-* #define SAMPBLK 4
-*/
-
-typedef struct _delay1 {
+typedef struct _delay1_cubic {
   t_object x_obj;
 
   t_float x_s_per_msec; // samples per msec
   t_float x_delay_buffer_msecs;
   int x_delay_buffer_samples; // number of samples in delay buffer
   int x_delay_msecs; // number of msecs to delay
-  int x_delay_samples; // number of samples of delay
+  t_float x_delay_samples; // number of samples of delay
   t_sample *x_delay_buffer;
   int x_pd_block_size;
   int x_phase; // current __write__ position
 
-} t_delay1;
+} t_delay1_cubic;
 
-t_class *delay1_class = NULL;
+t_class *delay1_cubic_class = NULL;
 
-static void delay_buffer_update(t_delay1 *x);
-static void delay_set_delay_samples(t_delay1 *x, t_float f);
+static void delay_buffer_update(t_delay1_cubic *x);
+static void delay_set_delay_samples(t_delay1_cubic *x, t_float f);
 
 static void *delay_new(t_floatarg buffer_msecs, t_floatarg delay_msecs)
 {
-  t_delay1 *x = (t_delay1 *)pd_new(delay1_class);
+  t_delay1_cubic *x = (t_delay1_cubic *)pd_new(delay1_cubic_class);
 
   x->x_delay_buffer_msecs = buffer_msecs;
   x->x_delay_msecs = delay_msecs;
@@ -53,7 +43,7 @@ static void *delay_new(t_floatarg buffer_msecs, t_floatarg delay_msecs)
   return (void *)x;
 }
 
-static void delay_buffer_update(t_delay1 *x)
+static void delay_buffer_update(t_delay1_cubic *x)
 {
   int nsamps = x->x_delay_buffer_msecs * x->x_s_per_msec;
   if (nsamps < 1) nsamps = 1;
@@ -74,53 +64,46 @@ static void delay_buffer_update(t_delay1 *x)
   }
 }
 
-static void delay_set_system_params(t_delay1 *x, int blocksize, t_float sr)
+static void delay_set_system_params(t_delay1_cubic *x, int blocksize, t_float sr)
 {
   x->x_pd_block_size = blocksize;
   x->x_s_per_msec = sr * 0.001f;
 }
 
-static void delay_set_delay_samples(t_delay1 *x, t_float f)
+static void delay_set_delay_samples(t_delay1_cubic *x, t_float f)
 {
-  // currently this is just reassigning x->x_delay_msecs to x->x_delay_msecs
+  // currently this is reassigning x->x_delay_msecs to x->x_delay_msecs
   // this approach might make sense later when x->delay_msecs can be set via a
   // message instead of just as a creating argument.
   x->x_delay_msecs = f;
-  x->x_delay_samples = (int)(0.5 + x->x_s_per_msec * x->x_delay_msecs);
+  x->x_delay_samples = x->x_s_per_msec * x->x_delay_msecs;
 }
 
-static t_int *delay1_perform(t_int *w)
+static t_int *delay1_cubic_perform(t_int *w)
 {
-  t_delay1 *x = (t_delay1*)(w[1]);
+  t_delay1_cubic *x = (t_delay1_cubic*)(w[1]);
   t_sample *in1 = (t_sample *)(w[2]);
   t_sample *out = (t_sample *)(w[3]);
   int n = (int)(w[4]);
 
+  int delay_buffer_samps = x->x_delay_buffer_samples;
+  int delay_samples_int = (int)x->x_delay_samples;
+
+  float delay_frac = x->x_delay_samples - delay_samples_int;
   int write_phase = x->x_phase;
   write_phase += n; // increment write position by block size for new loop
-  int read_phase = write_phase - x->x_delay_samples;
 
-  int delay_buffer_samps = x->x_delay_buffer_samples;
-
-  t_sample *vp = x->x_delay_buffer; // pointer to beginning of delay buffer
-  t_sample *wp = vp + write_phase; // pointer to current write position
-  t_sample *rp; // pointer to current read position
-  t_sample *ep = vp + (x->x_delay_buffer_samples + XTRASAMPS); // pointer to end
-  // of delay buffer
-
-  // if write_phase , x->x_delay_samples is before the beginning of the buffer
+  int read_phase = write_phase - delay_samples_int;
   if (read_phase < 0) read_phase += delay_buffer_samps; 
-  rp = vp + read_phase;
+
+  t_sample *vp = x->x_delay_buffer;
+  t_sample *wp = vp + write_phase;
+  t_sample *ep = vp + (x->x_delay_buffer_samples + XTRASAMPS);
 
   while (n--) {
-    // write input to delay buffer
-    // the output sounds clear, but does the read/write order matter?
     t_sample f = *in1++;
     if (PD_BIGORSMALL(f)) f = 0.0f;
     *wp++ = f;
-    // this is an attempt to copy what's happening in the Pure Data source code
-    // my understanding is that vp[0] to vp[3] only get written to here, but may
-    // be accessed by the read pointer
     if (wp == ep) {
       vp[0] = ep[-4];
       vp[1] = ep[-3];
@@ -130,24 +113,38 @@ static t_int *delay1_perform(t_int *w)
       write_phase -= delay_buffer_samps;
     }
 
-    *out++ = *rp++;
+    if (read_phase < XTRASAMPS) read_phase += delay_samples_int;
+    t_sample a = vp[read_phase];
+    t_sample b = vp[read_phase - 1];
+    t_sample c = vp[read_phase - 2];
+    t_sample d = vp[read_phase - 3];
+    t_sample cminusb = c-b;
 
-    if (rp == ep) rp -= delay_buffer_samps;
+    *out++ = b + delay_frac * (
+        cminusb - 0.1666667f * (1.-delay_frac) * (
+            (d - a - 3.0f * cminusb) * delay_frac + (d + 2.0f*a - 3.0f*b)
+        )
+    );
+
+    read_phase++;
+    if (read_phase > delay_buffer_samps) {
+      read_phase -= delay_buffer_samps;
+    }
   }
 
   x->x_phase = write_phase;
   return (w+5);
 }
 
-static void delay1_dsp(t_delay1 *x, t_signal **sp)
+static void delay1_cubic_dsp(t_delay1_cubic *x, t_signal **sp)
 {
-  dsp_add(delay1_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_length);
+  dsp_add(delay1_cubic_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_length);
   delay_set_system_params(x, sp[0]->s_length, sp[0]->s_sr);
   delay_buffer_update(x);
   delay_set_delay_samples(x, x->x_delay_msecs);
 }
 
-static void delay_free(t_delay1 *x)
+static void delay_free(t_delay1_cubic *x)
 {
   if (x->x_delay_buffer != NULL) {
     freebytes(x->x_delay_buffer,
@@ -156,17 +153,17 @@ static void delay_free(t_delay1 *x)
   }
 }
 
-void delay1_tilde_setup(void)
+void delay1_cubic_tilde_setup(void)
 {
-  delay1_class = class_new(gensym("delay1~"),
+  delay1_cubic_class = class_new(gensym("delay1_cubic~"),
                           (t_newmethod)delay_new,
                           (t_method)delay_free,
-                          sizeof(t_delay1),
+                          sizeof(t_delay1_cubic),
                           CLASS_DEFAULT,
                           A_DEFFLOAT, A_DEFFLOAT, 0);
 
-  class_addmethod(delay1_class, (t_method)delay1_dsp,
+  class_addmethod(delay1_cubic_class, (t_method)delay1_cubic_dsp,
                   gensym("dsp"), A_CANT, 0);
 
-  CLASS_MAINSIGNALIN(delay1_class, t_delay1, x_delay_buffer_msecs);
+  CLASS_MAINSIGNALIN(delay1_cubic_class, t_delay1_cubic, x_delay_buffer_msecs);
 }
